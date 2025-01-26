a2a55f42c346db5421fc8cd2213dbd0096d55421
decastyled <decastyled@gmail.com>
2025-01-26 15:28:24 +0500

    QVulkanWindow

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,33 +1,21 @@
 #include <QApplication>
 #include <QWidget>
+#include <QVulkanWindow>
 #include <vulkan/vulkan.h>
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
-    const char* appName = "discovering-path-tracer";
+    QVulkanInstance instance;
+    instance.setLayers({ "VK_LAYER_KHRONOS_validation" });
+    if (!instance.create())
+        qFatal("Failed to create Vulkan instance: %d", instance.errorCode());
 
-    QWidget window;
-    window.setWindowTitle(appName);
-    window.resize(800, 600);
+    QVulkanWindow window;
+    window.setVulkanInstance(&instance);
+
+    window.resize(1024, 768);
     window.show();
-
-    VkInstance instance;
-    VkApplicationInfo appInfo = {};
-    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-    appInfo.pApplicationName = appName;
-    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-    appInfo.pEngineName = "No Engine";
-    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-    appInfo.apiVersion = VK_API_VERSION_1_0;
-
-    VkInstanceCreateInfo createInfo = {};
-    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-    createInfo.pApplicationInfo = &appInfo;
-
-    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
-        qFatal("Failed to create Vulkan instance!");
-    }
 
     return app.exec();
 }

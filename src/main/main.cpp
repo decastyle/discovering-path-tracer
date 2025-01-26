b0213c7eb3ef744458786eb053832a0621ba51b9
decastyled <decastyled@gmail.com>
2025-01-26 09:18:59 +0500

    Basic window setup

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,7 +1,33 @@
-#include <iostream>
+#include <QApplication>
+#include <QWidget>
+#include <vulkan/vulkan.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cin.get();  // Waits for the user to press Enter
-    return 0;
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+
+    const char* appName = "discovering-path-tracer";
+
+    QWidget window;
+    window.setWindowTitle(appName);
+    window.resize(800, 600);
+    window.show();
+
+    VkInstance instance;
+    VkApplicationInfo appInfo = {};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = appName;
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo createInfo = {};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+        qFatal("Failed to create Vulkan instance!");
+    }
+
+    return app.exec();
 }
